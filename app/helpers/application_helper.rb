f09eb6c9ee64be3e7b960a823ad541effa3f2014
Robert Gingras <developer@three-point-five.dev>
2023-12-09 12:35:43 -0500

    bootstrap form helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  class BootstrapFormBuilder < ActionView::Helpers::FormBuilder
+    def text_field(attribute, options = {})
+      options[:class] = 'form-control'
+      options[:placeholder] = attribute.to_s
+      @template.content_tag :div, class: "form-floating" do
+        super + label(attribute)
+      end
+    end
+
+    def text_area(attribute, options = {})
+      options[:class] = 'form-control'
+      options[:placeholder] = attribute.to_s
+      @template.content_tag :div, class: 'form-floating' do
+        super + label(attribute)
+      end
+    end
+
+    def submit(value = nil, options = {})
+      options[:class] = 'btn btn-primary'
+      super
+    end
+  end
 end
