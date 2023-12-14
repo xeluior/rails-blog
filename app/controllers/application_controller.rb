6f3d683e6dace6b1fdc246ad5a45d30a7c45df9e
Robert Gingras <developer@three-point-five.dev>
2023-12-13 21:17:53 -0500

    pundit exception handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: %i[fullname])
+  end
+  
+  private
+
+  def user_not_authorized
+    flash[:alert] = 'You are not authorized for that resource.'
+    redirect_to :root
   end
 end
